598fe7ee557f3821771635b80f7949935a8cfec3
ssshun <shun20214@gmail.com>
2020-02-01 22:51:31 +0900

    devide standby screen from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:digital_book_shelf/home.dart';
+import 'package:digital_book_shelf/standby_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,40 +20,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: StandbyScreen(),
-    );
-  }
-}
-
-class StandbyScreen extends StatefulWidget {
-  @override
-  _StandbyScreenState createState() => _StandbyScreenState();
-}
-
-class _StandbyScreenState extends State<StandbyScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Home(),
-              ),
-            );
-          },
-          child: Container(
-            constraints: BoxConstraints.expand(),
-            child: CachedNetworkImage(
-              imageUrl:
-                  'https://firebasestorage.googleapis.com/v0/b/digital-bookshelf-9da55.appspot.com/o/book_image%2F1.jpg?alt=media&token=faa961c8-4272-4515-b0d7-e32a8e6578d2',
-              errorWidget: (context, url, error) => Icon(Icons.error),
-              fit: BoxFit.contain,
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
