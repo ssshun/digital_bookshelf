044ab9f6ad89faf7d5127c99fe515e1d09568d83
ssshun <shun20214@gmail.com>
2020-02-01 22:49:08 +0900

    add standby screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:digital_book_shelf/ranking.dart';
-import 'package:digital_book_shelf/search.dart';
-import 'package:digital_book_shelf/setting.dart';
+import 'package:digital_book_shelf/home.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,69 +18,43 @@ class MyApp extends StatelessWidget {
         Locale('ja', 'JP'),
       ],
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: App(),
+      home: StandbyScreen(),
     );
   }
 }
 
-class App extends StatefulWidget {
+class StandbyScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => AppState();
+  _StandbyScreenState createState() => _StandbyScreenState();
 }
 
-class AppState extends State<App> {
-  List<Widget> _pages = [
-    Ranking(),
-    Search(),
-    Setting(),
-  ];
-  final iconSize = 28.0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class _StandbyScreenState extends State<StandbyScreen> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            title: Text('ランキング'),
-            icon: Icon(FontAwesomeIcons.star, size: iconSize),
-          ),
-          BottomNavigationBarItem(
-            title: Text('検索'),
-            icon: Icon(FontAwesomeIcons.search, size: iconSize),
-          ),
-          BottomNavigationBarItem(
-            title: Text('設定'),
-            icon: Icon(FontAwesomeIcons.user, size: iconSize),
-          ),
-        ],
-        backgroundColor: Colors.white,
-      ),
-      tabBuilder: (BuildContext context, int index) {
-        return new CupertinoTabView(
-          builder: (BuildContext context) {
-            return CupertinoPageScaffold(
-              child: _pages[index],
+    return Scaffold(
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Home(),
+              ),
             );
           },
-        );
-      },
+          child: Container(
+            constraints: BoxConstraints.expand(),
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://firebasestorage.googleapis.com/v0/b/digital-bookshelf-9da55.appspot.com/o/book_image%2F1.jpg?alt=media&token=faa961c8-4272-4515-b0d7-e32a8e6578d2',
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
